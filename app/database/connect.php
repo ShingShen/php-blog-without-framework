db777d9361cbb06fa53aa247a8291f31f100c321
shingshen <seeeyetoeye314@gmail.com>
2021-05-19 22:32:14 +0800

    version2

--- app/database/connect.php
+++ app/database/connect.php
@@ -3,10 +3,27 @@
 $host = 'localhost';
 $user = 'root';
 $pass = '';
-$db_name = 'blog';
+$db_name = 'blog'; //the name do be same as database
 
 $conn = new MySQLi($host, $user, $pass, $db_name);
 
 if ($conn->connect_error){
     die('Database connection error: ' . $conn->connect_error);
-}
+} else {
+    echo "Database connection is successful!";
+} 
+
+/*
+create the database called blog in phpmyadmin,
+and table called users in the database.
+
+The table has to contain following contents:
+
+id: Type- INT Length/Valuses- 11 Index- PRIMARY A_I YES
+admin: Type- TINYINT
+username: Type- VARCHAR Length/Valuses- 255
+email: Type- VARCHAR Length/Valuses- 255 Index- UNIQUE
+password: Type- VARCHAR Length/Valuses- 255
+create_at: Type- TIMESTAMP Default- CURRENT_TIMESTAMP
+
+*/
--- path.php
+++ path.php
@@ -1,3 +1,6 @@
 <?php
 define('ROOT_PATH', realpath(dirname(__FILE__)));
 define('BASE_URL', "http://localhost/blog");
+
+var_dump(ROOT_PATH);
+var_dump(BASE_URL);
